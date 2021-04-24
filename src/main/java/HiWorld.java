f070d101095059ef1ac7d912b9eb2ef1290c09af
张正 <1209986709@qq.com>
2021-04-24 15:43:28 +0800

    test2

--- src/main/java/HiWorld.java
+++ src/main/java/HiWorld.java
@@ -2,7 +2,6 @@ public class HiWorld {
 
     public static void main(String[] args) {
         System.out.println("Hi World");
-        System.out.println("Hi World1222");
-
+        System.out.println("Hi World1222666");
     }
 }
